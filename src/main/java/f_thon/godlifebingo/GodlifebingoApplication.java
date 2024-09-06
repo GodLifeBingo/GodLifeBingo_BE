54f50f57eb71da7facf875e7f928a54d9698d00a
tlsrl6427 <tlsr6427@naver.com>
2024-09-07 03:25:55 +0900

    init

---
+++ src/main/java/f_thon/godlifebingo/GodlifebingoApplication.java
@@ -0,0 +1,13 @@
+package f_thon.godlifebingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodlifebingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodlifebingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/f_thon/godlifebingo/GodlifebingoApplicationTests.java
@@ -0,0 +1,13 @@
+package f_thon.godlifebingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodlifebingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
