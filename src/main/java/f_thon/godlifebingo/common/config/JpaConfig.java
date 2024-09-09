fe19dc5dbd3c7d296e92e45f764811741be727bd
tlsrl6427 <tlsr6427@naver.com>
2024-09-10 00:04:16 +0900

    Feat: JpaConfig (BaseEntity 기능 허용)

---
+++ src/main/java/f_thon/godlifebingo/common/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package f_thon.godlifebingo.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
