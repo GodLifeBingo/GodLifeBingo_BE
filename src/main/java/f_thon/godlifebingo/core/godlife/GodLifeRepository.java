e1e5dd4309dcdcdc1efd3d0a16b0e2475d56a51d
jhkim31 <biz.jh.kim@gmail.com>
2024-09-11 15:59:18 +0900

    FEAT: 갓생 통계 기능 * 모든 유저는 모든 갓생의 통계를 확인할 수 있다 * 갓생 통계의 개인 달성율은 개인의 모든 갓생 기록을 합산한 결과 * 전체 달성율은 모든 유저의 갓생 기록을 합산한 결과

--- src/main/java/f_thon/godlifebingo/core/godlife/GodLifeRepository.java
+++ src/main/java/f_thon/godlifebingo/core/godlife/GodLifeRepository.java
@@ -1,9 +1,25 @@
 package f_thon.godlifebingo.core.godlife;
 
-import f_thon.godlifebingo.core.godlife.SimpleGodLifeListResponse.SimpleGodLifeResponse;
-import java.util.List;
+import f_thon.godlifebingo.core.users.Users;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.parameters.P;
 
 public interface GodLifeRepository extends JpaRepository<GodLife, Long> {
+
+    @Query("""
+         select
+           new f_thon.godlifebingo.core.godlife.GodLifeStatistic(gl.title, gl.id, sum(c.currentProgress), sum(b.totalCount), gl.allUserCompletedRate)
+         from
+           GodLife  gl
+         left join Cell c on c.godlife = gl
+         left join Bingo b on c.bingo = b and b.users = :user
+         where
+           gl.title like %:query%          
+         group by 
+           gl.id     
+        """)
+    Page<GodLifeStatistic> getGodLifeStatistics(@P("query") String query, @P("user")Users user, Pageable pageable);
 }
--- src/main/java/f_thon/godlifebingo/core/godlife/GodLifeService.java
+++ src/main/java/f_thon/godlifebingo/core/godlife/GodLifeService.java
@@ -1,8 +1,12 @@
 package f_thon.godlifebingo.core.godlife;
 
 import f_thon.godlifebingo.core.godlife.SimpleGodLifeListResponse.SimpleGodLifeResponse;
+import f_thon.godlifebingo.core.users.Users;
+import f_thon.godlifebingo.core.users.UsersRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class GodLifeService {
 
     private final GodLifeRepository godLifeRepository;
+    private final UsersRepository usersRepository;
 
     public SimpleGodLifeListResponse getAllGodLifesSimple() {
         List<GodLife> godlifes = godLifeRepository.findAll();
@@ -23,10 +28,24 @@ public class GodLifeService {
                 .build())
             .toList();
 
-        SimpleGodLifeListResponse godLifeListResponse = SimpleGodLifeListResponse.builder()
+        return SimpleGodLifeListResponse.builder()
             .godLifes(simpleGodlifes)
             .build();
+    }
 
-        return godLifeListResponse;
+    public GodLifeStatisticsListResponse getGodLifeStatisticsList(String searchTitle,
+        Pageable pageable, Long userId) {
+        Users user = usersRepository.getReferenceById(userId);
+
+        Page<GodLifeStatistic> godLifeStatistics = godLifeRepository.getGodLifeStatistics(
+            searchTitle, user, pageable);
+
+        return GodLifeStatisticsListResponse.builder()
+            .godlifes(godLifeStatistics.getContent())
+            .page(pageable.getPageNumber())
+            .size(pageable.getPageSize())
+            .totalPage(godLifeStatistics.getTotalPages())
+            .totalCount(godLifeStatistics.getTotalElements())
+            .build();
     }
 }
