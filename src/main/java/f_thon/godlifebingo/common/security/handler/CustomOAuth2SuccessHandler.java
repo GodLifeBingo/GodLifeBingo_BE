30921c5b16e7c944f526b00e596c021466ae4b8b
tlsrl6427 <tlsr6427@naver.com>
2024-09-13 01:43:23 +0900

    TEST

--- src/main/java/f_thon/godlifebingo/common/security/handler/CustomOAuth2SuccessHandler.java
+++ src/main/java/f_thon/godlifebingo/common/security/handler/CustomOAuth2SuccessHandler.java
@@ -21,9 +21,9 @@ public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHa
         Cookie cookie = new Cookie("JSESSIONID", sessionId);
         cookie.setSecure(true);  // HTTPS 사용 시 필수
         cookie.setHttpOnly(true); // 클라이언트 스크립트로부터 쿠키 보호
-        cookie.setPath("/");
+//        cookie.setPath("/");
         cookie.setMaxAge(60 * 60); // 쿠키의 유효시간 설정
-        cookie.setDomain("*.vercel.app");
+//        cookie.setDomain("*.vercel.app");
 
         response.addCookie(cookie);
         response.sendRedirect("https://god-life-bingo.vercel.app");
