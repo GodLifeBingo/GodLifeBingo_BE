2d5d042b54dee879b1839dc0f8da4ce415bfc3aa
tlsrl6427 <tlsr6427@naver.com>
2024-09-13 20:30:58 +0900

    TEST

--- src/main/java/f_thon/godlifebingo/core/bingo/BingoRepository.java
+++ src/main/java/f_thon/godlifebingo/core/bingo/BingoRepository.java
@@ -8,12 +8,12 @@ import org.springframework.data.repository.query.Param;
 
 public interface BingoRepository extends JpaRepository<Bingo, Long>, BingoCustomRepository {
 
-    @Modifying
+    /*@Modifying
     @Query("UPDATE bingo b, (SELECT b.id as bingo_id, SUM(c.current_progress / b.total_count) /  COUNT(b.id) * 100 as completed_rate"
         + " FROM bingo b"
         + " JOIN cell c ON b.id = c.bingo_id"
         + " GROUP BY b.id ) as tmp"
         + " SET b.total_completed_rate = tmp.completed_rate"
         + " where b.id = tmp.bingo_id")
-    void updateTotalCompletedRate();
+    void updateTotalCompletedRate();*/
 }
